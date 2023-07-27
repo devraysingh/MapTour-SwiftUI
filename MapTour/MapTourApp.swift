5539d963e51ac5bb2f7a45ed750191d20676248a
devraysingh <raysahni98@gmail.com>
2023-07-27 00:58:44 -0400

    Initial Commit

---
+++ MapTour/MapTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTourApp.swift
+//  MapTour
+//
+//  Created by ray on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MapTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
